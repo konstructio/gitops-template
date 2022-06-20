331ebd052bc4ce2ecb49844a8812ef6d90f059a9
John Dietz <john@kubefirst.com>
2022-06-20 13:56:21 -0400

    provides the ability to create tokens from kubectl during init

---
+++ components/gitlab-init/gitlab-init.sh
@@ -0,0 +1,2 @@
+export GITLAB_TOKEN=$1
+gitlab-rails runner "token = User.find_by_username('root').personal_access_tokens.create(scopes: [:write_registry, :write_repository, :api], name: 'Automation token'); token.set_token(ENV['GITLAB_TOKEN']); token.save!"
