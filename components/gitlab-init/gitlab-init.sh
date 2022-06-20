b35a93a6151469eefae7ce3eed8f2892df082cab
johndietz <john@kubefirst.com>
2022-06-20 14:40:36 -0400

    addding +x and shebang to gitlab-init.sh

--- components/gitlab-init/gitlab-init.sh
+++ components/gitlab-init/gitlab-init.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 export GITLAB_TOKEN=$1
 gitlab-rails runner "token = User.find_by_username('root').personal_access_tokens.create(scopes: [:write_registry, :write_repository, :api], name: 'Automation token'); token.set_token(ENV['GITLAB_TOKEN']); token.save!"
