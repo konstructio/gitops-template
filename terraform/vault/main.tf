9393c02e01257c6f94ee320786c1b72c86c241f6
jarededwards <jared@kubefirst.com>
2022-06-21 21:00:57 -0600

    remove unused vars and cleanup cypress

--- terraform/vault/main.tf
+++ terraform/vault/main.tf
@@ -12,18 +12,12 @@ module "bootstrap" {
 
   aws_account_id                    = var.aws_account_id
   aws_region                        = var.aws_region
-  aws_secret_access_key             = var.aws_secret_access_key
   vault_token                       = var.vault_token
   email_address                     = var.email_address
   vault_addr                        = var.vault_addr
-  aws_access_key_id                 = var.aws_access_key_id
-  gitlab_bot_root_password          = var.gitlab_bot_root_password
   hosted_zone_id                    = var.hosted_zone_id
   hosted_zone_name                  = var.hosted_zone_name
   gitlab_runner_token               = var.gitlab_runner_token
-  argocd_auth_password              = var.argocd_auth_password
-  atlantis_gitlab_token             = var.atlantis_gitlab_token
-  atlantis_gitlab_webhook_secret    = var.atlantis_gitlab_webhook_secret
   gitlab_token                      = var.gitlab_token
 }
 
