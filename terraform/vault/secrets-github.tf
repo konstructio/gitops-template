0d95163e9d096971c872b760adf593b5039e87a4
Jared Edwards <jared@kubefirst.com>
2022-10-09 22:50:28 -0600

    add missing comma

--- terraform/vault/secrets-github.tf
+++ terraform/vault/secrets-github.tf
@@ -25,7 +25,7 @@ resource "vault_generic_secret" "atlantis_secrets" {
   		"ATLANTIS_GH_HOSTNAME": "<GITHUB_HOST>",
   		"ATLANTIS_GH_TOKEN": "${var.github_token}",
   		"ATLANTIS_GH_USER": "<GITHUB_USER>",
-      "ATLANTIS_GH_WEBHOOK_SECRET":  "${var.atlantis_repo_webhook_secret}",
+	        "ATLANTIS_GH_WEBHOOK_SECRET":  "${var.atlantis_repo_webhook_secret}",
   		"AWS_DEFAULT_REGION": "<AWS_DEFAULT_REGION>",
   		"AWS_ROLE_TO_ASSUME": "arn:aws:iam::<AWS_ACCOUNT_ID>:role/KubernetesAdmin",
   		"AWS_SESSION_NAME": "GitHubAction",
@@ -35,15 +35,15 @@ resource "vault_generic_secret" "atlantis_secrets" {
   		"TF_VAR_atlantis_repo_webhook_secret": "${var.atlantis_repo_webhook_secret}",
   		"TF_VAR_aws_account_id": "<AWS_ACCOUNT_ID>",
   		"TF_VAR_aws_region": "<AWS_DEFAULT_REGION>",
-      "TF_VAR_email_address": "${var.email_address}",
-  		"TF_VAR_github_token": "${var.github_token}"
+      		"TF_VAR_email_address": "${var.email_address}",
+  		"TF_VAR_github_token": "${var.github_token}",
   		"TF_VAR_hosted_zone_id": "${var.hosted_zone_id}",
   		"TF_VAR_hosted_zone_name": "${var.hosted_zone_name}",
   		"TF_VAR_kubefirst_bot_ssh_public_key": "${var.kubefirst_bot_ssh_public_key}",
-      "TF_VAR_ssh_private_key": "${var.ssh_private_key}",
-      "TF_VAR_vault_addr": "${var.vault_addr}",
+      		"TF_VAR_ssh_private_key": "${var.ssh_private_key}",
+      		"TF_VAR_vault_addr": "${var.vault_addr}",
   		"TF_VAR_vault_token": "${var.vault_token}",
-      "VAULT_ADDR": "https://vault.<AWS_HOSTED_ZONE_NAME>",
+      		"VAULT_ADDR": "https://vault.<AWS_HOSTED_ZONE_NAME>",
   		"VAULT_TOKEN": "${var.vault_token}"
   	}
 }
