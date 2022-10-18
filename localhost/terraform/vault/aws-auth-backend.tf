bfe0ca9b54c44d77e0c46791713f650969c49704
Jared Edwards <jared@kubefirst.com>
2022-10-18 14:59:25 -0600

    remove aws backend

--- localhost/terraform/vault/aws-auth-backend.tf
+++ localhost/terraform/vault/aws-auth-backend.tf
@@ -7,21 +7,3 @@ resource "vault_auth_backend" "aws" {
   type = "aws"
   path = "aws"
 }
-
-resource "vault_aws_secret_backend" "aws" {}
-
-resource "vault_aws_secret_backend_role" "admin" {
-  backend         = vault_auth_backend.aws.path
-  name            = "KubernetesAdmin"
-  credential_type = "assumed_role"
-
-  role_arns = ["arn:aws:iam::<AWS_ACCOUNT_ID>:role/KubernetesAdmin"]
-}
-
-resource "vault_aws_secret_backend_role" "developer" {
-  backend         = vault_auth_backend.aws.path
-  name            = "KubernetesDeveloper"
-  credential_type = "assumed_role"
-
-  role_arns = ["arn:aws:iam::<AWS_ACCOUNT_ID>:role/KubernetesDeveloper"]
-}
