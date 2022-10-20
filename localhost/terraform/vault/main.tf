011a2487554a63a49718249b40aca909d41b813a
João Vanzuita <joao@kubeshop.io>
2022-10-20 16:29:58 -0300

    chore: update minio tfstate endpoint
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

--- localhost/terraform/vault/main.tf
+++ localhost/terraform/vault/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "kubefirst-state-store"
     key     = "terraform/vault/tfstate.tf"
-    endpoint = "http://127.0.0.1:9000"
+    endpoint = "http://minio.minio.svc.cluster.local:9000"
 
     access_key="k-ray"
     secret_key="feedkraystars"
