dc241c7eb57f1336925ddc465d5b7c1ba3aa187b
Jessica Marinho <jessica@kubeshop.io>
2022-10-26 14:00:59 -0300

    Change vault port
    Signed-off-by: Jessica Marinho <jessica@kubeshop.io>

--- localhost/terraform/vault/secrets-github.tf
+++ localhost/terraform/vault/secrets-github.tf
@@ -31,9 +31,9 @@ resource "vault_generic_secret" "atlantis_secrets" {
       TF_VAR_atlantis_repo_webhook_secret = var.atlantis_repo_webhook_secret,
       TF_VAR_github_token                 = var.github_token,
       TF_VAR_kubefirst_bot_ssh_public_key = var.kubefirst_bot_ssh_public_key,
-      TF_VAR_vault_addr                   = "http://vault.vault.svc.cluster.local:443",
+      TF_VAR_vault_addr                   = "http://vault.vault.svc.cluster.local:8200",
       TF_VAR_vault_token                  = "k1_local_vault_token",
-      VAULT_ADDR                          = "http://vault.vault.svc.cluster.local:443",
+      VAULT_ADDR                          = "http://vault.vault.svc.cluster.local:8200",
       VAULT_TOKEN                         = "k1_local_vault_token",
     }
   )
