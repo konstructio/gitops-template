16805f8f3bba1cb6c858d62ee67f957168faa575
Jared Edwards <jared@kubefirst.com>
2022-11-30 18:31:32 -0700

    add metaphor to repos

--- aws-github/terraform/github/repos.tf
+++ aws-github/terraform/github/repos.tf
@@ -41,3 +41,37 @@ variable "atlantis_repo_webhook_secret" {
   type = string
   default = ""
 }
+
+
+module "metaphor" {
+  source = "./modules/repository"
+
+  repo_name          = "metaphor"
+  archive_on_destroy = false
+  auto_init          = false # set to false if importing an existing repository
+  create_ecr         = true
+  team_developers_id = github_team.developers.id
+  team_admins_id     = github_team.admins.id
+}
+
+module "metaphor_go" {
+  source = "./modules/repository"
+
+  repo_name          = "metaphor-go"
+  archive_on_destroy = false
+  auto_init          = false # set to false if importing an existing repository
+  create_ecr         = true
+  team_developers_id = github_team.developers.id
+  team_admins_id     = github_team.admins.id
+}
+
+module "metaphor_frontend" {
+  source = "./modules/repository"
+
+  repo_name          = "metaphor-frontend"
+  archive_on_destroy = false
+  auto_init          = false # set to false if importing an existing repository
+  create_ecr         = true
+  team_developers_id = github_team.developers.id
+  team_admins_id     = github_team.admins.id
+}
