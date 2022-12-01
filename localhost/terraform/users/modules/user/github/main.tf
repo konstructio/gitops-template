c5bfca9df23c88ff49b34f7858c52a842f93507a
Cesar Filho <53096417+6za@users.noreply.github.com>
2022-12-01 16:21:59 -0300

    Update main.tf

--- localhost/terraform/users/modules/user/github/main.tf
+++ localhost/terraform/users/modules/user/github/main.tf
@@ -27,7 +27,9 @@ resource "random_password" "password" {
 resource "vault_generic_endpoint" "user" {
   path                 = "auth/userpass/users/${var.username}"
   ignore_absent_fields = true
-
+  lifecycle {
+    ignore_changes = [ data_json ] 
+   }
   data_json = jsonencode(
     {
       policies  = var.acl_policies,
@@ -39,7 +41,9 @@ resource "vault_generic_endpoint" "user" {
 
 resource "vault_generic_secret" "user" {
   path = "users/${var.username}"
-
+  lifecycle {
+    ignore_changes = [ data_json ] 
+   }
   data_json = <<EOT
 {
   "initial-password": "${var.initial_password != "" ? var.initial_password : random_password.password.result}"
