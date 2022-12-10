dd29800854c146d15221d2500087f6be969eaae0
Jared Edwards <jared@kubefirst.com>
2022-12-09 19:15:59 -0700

    forgot to add output for repo name

--- terraform/github/repos.tf
+++ terraform/github/repos.tf
@@ -25,19 +25,53 @@ module "gitops" {
 
 resource "github_repository_webhook" "gitops_atlantis_webhook" {
     repository = module.gitops.repo_name
-  
+
     configuration {
       url          = "https://atlantis.<AWS_HOSTED_ZONE_NAME>/events"
       content_type = "json"
       insecure_ssl = false
       secret       = var.atlantis_repo_webhook_secret
     }
-  
+
     active = true
-  
+
     events = ["pull_request_review", "push", "issue_comment", "pull_request"]
 }
 variable "atlantis_repo_webhook_secret" {
   type = string
   default = ""
+}
+
+
+module "metaphor" {
+  source = "./modules/repository"
+
+  repo_name          = "metaphor"
+  archive_on_destroy = false
+  auto_init          = false # set to false if importing an existing repository
+  create_ecr         = true
+  team_developers_id = github_team.developers.id
+  team_admins_id     = github_team.admins.id
+}
+
+module "metaphor_go" {
+  source = "./modules/repository"
+
+  repo_name          = "metaphor-go"
+  archive_on_destroy = false
+  auto_init          = false
+  create_ecr         = true
+  team_developers_id = github_team.developers.id
+  team_admins_id     = github_team.admins.id
+}
+
+module "metaphor_frontend" {
+  source = "./modules/repository"
+
+  repo_name          = "metaphor-frontend"
+  archive_on_destroy = false
+  auto_init          = false
+  create_ecr         = true
+  team_developers_id = github_team.developers.id
+  team_admins_id     = github_team.admins.id
 }
