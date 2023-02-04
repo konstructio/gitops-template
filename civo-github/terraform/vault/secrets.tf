44ce4c4eaadc7c97f5f5ffa5b56c1bbe26039127
Jared Edwards <jared@kubefirst.com>
2023-02-04 00:14:20 -0700

    remove unused secrets

--- civo-github/terraform/vault/secrets.tf
+++ civo-github/terraform/vault/secrets.tf
@@ -87,10 +87,6 @@ resource "vault_generic_secret" "ci_secrets" {
     {
       BASIC_AUTH_USER       = "k-ray",
       BASIC_AUTH_PASS       = "feedkraystars",
-      USERNAME              = "<GITHUB_USER>",
-      PERSONAL_ACCESS_TOKEN = var.github_token,
-      username              = "<GITHUB_USER>",
-      password              = var.github_token,
       SSH_PRIVATE_KEY       = var.kubefirst_bot_ssh_private_key,
     }
   )
@@ -105,22 +101,26 @@ resource "vault_generic_secret" "atlantis_secrets" {
       ARGOCD_INSECURE                     = "true",
       ARGOCD_SERVER                       = "http://localhost:8080",
       ARGO_SERVER_URL                     = "argo.argo.svc.cluster.local:2746",
-      AWS_ACCESS_KEY_ID                   = var.aws_access_key_id,
-      AWS_SECRET_ACCESS_KEY               = var.aws_secret_access_key,
       ATLANTIS_GH_HOSTNAME                = "github.com",
       ATLANTIS_GH_TOKEN                   = var.github_token,
       ATLANTIS_GH_USER                    = "<GITHUB_USER>",
       ATLANTIS_GH_WEBHOOK_SECRET          = var.atlantis_repo_webhook_secret,
+      TF_VAR_atlantis_repo_webhook_secret = var.atlantis_repo_webhook_secret,
+      AWS_SECRET_ACCESS_KEY               = var.aws_secret_access_key,
+      TF_VAR_aws_secret_access_key        = var.aws_secret_access_key,
+      AWS_ACCESS_KEY_ID                   = var.aws_access_key_id,
+      TF_VAR_aws_access_key_id            = var.aws_access_key_id,
+      CIVO_TOKEN                          = var.civo_token,
+      TF_VAR_civo_token                   = var.civo_token,
       GITHUB_OWNER                        = "<GITHUB_OWNER>",
       GITHUB_TOKEN                        = var.github_token,
-      TF_VAR_atlantis_repo_webhook_secret = var.atlantis_repo_webhook_secret,
-      TF_VAR_atlantis_repo_webhook_url    = var.atlantis_repo_webhook_url,
       TF_VAR_github_token                 = var.github_token,
       TF_VAR_kubefirst_bot_ssh_public_key = var.kubefirst_bot_ssh_public_key,
-      TF_VAR_vault_addr                   = "http://vault.vault.svc.cluster.local:8200",
-      TF_VAR_vault_token                  = "k1_local_vault_token",
+      TF_VAR_atlantis_repo_webhook_url    = var.atlantis_repo_webhook_url,
       VAULT_ADDR                          = "http://vault.vault.svc.cluster.local:8200",
+      TF_VAR_vault_addr                   = "http://vault.vault.svc.cluster.local:8200",
       VAULT_TOKEN                         = "k1_local_vault_token",
+      TF_VAR_vault_token                  = "k1_local_vault_token",
     }
   )
 }
