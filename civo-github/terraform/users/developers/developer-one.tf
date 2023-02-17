4b4ac9245a52ee06422c0dc37adb37cbbe7679be
John Dietz <john@kubefirst.com>
2023-02-17 10:24:58 -0500

    Update developer-one.tf

--- civo-github/terraform/users/developers/developer-one.tf
+++ civo-github/terraform/users/developers/developer-one.tf
@@ -1,10 +1,14 @@
+# # note: uncomment the below to create a new developer, and be sure to
+# # adjust module name developer_one below to your developer's firstname_lastname.
+# # create as many developer module files as you have developer personnel.
+
 # module "developer_one" {
 #   source = "./modules/user/github"
 # 
 #   acl_policies            = ["developer"]
 #   email                   = "dev.one@example.com"
 #   first_name              = "Dev"
-#   github_username         = "dev-ones-github-handle"
+#   github_username         = "developer-ones-github-handle"
 #   team_id                 = data.github_team.developers.id
 #   last_name               = "One"
 #   username                = "done"
