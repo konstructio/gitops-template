42ba361810bc99cc04bfbac3f64bb4d795b8de95
John Dietz <john@kubefirst.com>
2023-02-17 10:23:10 -0500

    Update admin-one.tf

--- civo-github/terraform/users/admins/admin-one.tf
+++ civo-github/terraform/users/admins/admin-one.tf
@@ -1,8 +1,6 @@
-# # note: when you uncomment and change admin_one below 
-# # to your admin's firstname_lastname, you must also uncomment 
-# # and change the "admins_membership" list above to match your
-# # individual's firstname_lastname. create as many admin modules
-# # as you have admin personnel.
+# # note: uncomment the below to create a new admin, and be sure to
+# # adjust module name admin_one below to your admin's firstname_lastname.
+# # create as many admin modules files as you have admin personnel.
   
 # module "admin_one" {
 #   source = "./modules/user/github"
