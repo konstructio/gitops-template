1630b81d4ab58eff3832c87be93020a7035da98b
John Dietz <john@kubefirst.com>
2023-02-17 00:09:09 -0500

    update gitlab onboarding guidance and kbot brand

--- terraform/users/admins-gitlab.tf
+++ terraform/users/admins-gitlab.tf
@@ -4,7 +4,8 @@ data "vault_identity_group" "admins" {
 
 resource "vault_identity_group_member_entity_ids" "admin_membership" {
   member_entity_ids = [
-    module.kubefirst_bot.vault_identity_entity_id
+    module.kbot.vault_identity_entity_id
+#    , module.admin_one.vault_identity_entity_id      
   ]
 
   group_id = data.vault_identity_group.admins.group_id
@@ -15,7 +16,9 @@ variable "initial_password" {
   default = ""
 }
 
-module "kubefirst_bot" {
+module "kbot" {
+  # kbot is your automation user for all automation
+  # on the platform that needs a bot account
   source = "./modules/user/gitlab"
 
   acl_policies      = ["admin"]
@@ -30,6 +33,12 @@ module "kubefirst_bot" {
   userpass_accessor = data.vault_auth_backend.userpass.accessor
 }
 
+# # note: when you uncomment and change admin_one below 
+# # to your admin's firstname_lastname, you must also uncomment 
+# # and change the "admin_membership" list above to match your
+# # individual's firstname_lastname. create as many admin modules
+# # as you have admin personnel.
+  
 # module "admin_one" {
 #   source = "./modules/user/gitlab"
 
