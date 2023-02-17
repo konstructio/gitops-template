7787e1687ed9752d251eb30a019bcfa7fdf87371
Scott Hawkins <scott@echoboomer.net>
2023-02-17 13:57:14 -0500

    Add atlantis

--- civo-github/terraform/vault/kubernetes-auth-backend.tf
+++ civo-github/terraform/vault/kubernetes-auth-backend.tf
@@ -15,6 +15,39 @@ resource "vault_auth_backend" "k8s" {
   path = "kubernetes/kubefirst"
 }
 
+// atlantis
+
+data "kubernetes_service_account" "atlantis" {
+  metadata {
+    name      = "atlantis"
+    namespace = "atlantis"
+  }
+}
+
+data "kubernetes_secret" "atlantis_token_secret" {
+  metadata {
+    name      = data.kubernetes_service_account.atlantis.default_secret_name
+    namespace = "atlantis"
+  }
+}
+
+resource "vault_kubernetes_auth_backend_config" "vault_k8s_auth_atlantis" {
+  backend            = vault_auth_backend.k8s.path
+  kubernetes_host    = data.civo_kubernetes_cluster.kubefirst.api_endpoint
+  token_reviewer_jwt = data.kubernetes_secret.atlantis_token_secret.data.token
+}
+
+resource "vault_kubernetes_auth_backend_role" "k8s_atlantis" {
+  backend                          = vault_auth_backend.k8s.path
+  role_name                        = "atlantis"
+  bound_service_account_names      = ["atlantis"]
+  bound_service_account_namespaces = ["*"]
+  token_ttl                        = 86400
+  token_policies                   = ["admin", "default"]
+}
+
+// external-secrets
+
 data "kubernetes_service_account" "external_secrets" {
   metadata {
     name      = "external-secrets"
@@ -29,7 +62,7 @@ data "kubernetes_secret" "external_secrets_token_secret" {
   }
 }
 
-resource "vault_kubernetes_auth_backend_config" "vault_k8s_auth" {
+resource "vault_kubernetes_auth_backend_config" "vault_k8s_auth_es" {
   backend            = vault_auth_backend.k8s.path
   kubernetes_host    = data.civo_kubernetes_cluster.kubefirst.api_endpoint
   token_reviewer_jwt = data.kubernetes_secret.external_secrets_token_secret.data.token
