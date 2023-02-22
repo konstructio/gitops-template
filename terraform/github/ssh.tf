0852b7cfe3c067ac1f4fcf4b08a125462cc88c9e
Jared Edwards <jared@kubefirst.com>
2023-02-22 11:09:20 -0700

    adopt new structure

--- terraform/github/ssh.tf
+++
@@ -1,9 +0,0 @@
-resource "github_user_ssh_key" "kubefirst_bot" {
-  title = "kubefirst-bot"
-  key   = var.kubefirst_bot_ssh_public_key
-}
-
-variable "kubefirst_bot_ssh_public_key" {
-  type    = string
-  default = ""
-}
