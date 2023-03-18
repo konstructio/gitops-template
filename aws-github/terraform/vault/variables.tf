e7cbfb5d7804e915772bfc1623dfe69f77499845
Jared Edwards <jared@kubefirst.com>
2023-03-18 06:22:28 -0600

    add vault_token variable (#388)

--- aws-github/terraform/vault/variables.tf
+++ aws-github/terraform/vault/variables.tf
@@ -26,12 +26,7 @@ variable "atlantis_repo_webhook_url" {
   type    = string
 }
 
-# variable "aws_access_key_id" {
-#   default = ""
-#   type    = string
-# }
-
-# variable "aws_secret_access_key" {
-#   default = ""
-#   type    = string
-# }
+variable "vault_token" {
+  default = ""
+  type    = string
+}
