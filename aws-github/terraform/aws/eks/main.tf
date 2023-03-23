35451aac0091e4790384cb7431833279159b2897
John Dietz <john@kubefirst.com>
2023-03-23 15:49:57 -0400

    Update main.tf

--- aws-github/terraform/aws/eks/main.tf
+++ aws-github/terraform/aws/eks/main.tf
@@ -541,26 +541,26 @@ resource "aws_iam_policy" "vault_server" {
 EOT
 }
 
-module "ebs_kms_key" {
-  source  = "terraform-aws-modules/kms/aws"
-  version = "~> 1.5"
+# module "ebs_kms_key" {
+#   source  = "terraform-aws-modules/kms/aws"
+#   version = "~> 1.5"
 
-  description = "Customer managed key to encrypt EKS managed node group volumes"
+#   description = "Customer managed key to encrypt EKS managed node group volumes"
 
-  # Policy
-  key_administrators = [
-    data.aws_caller_identity.current.arn
-  ]
+#   # Policy
+#   key_administrators = [
+#     data.aws_caller_identity.current.arn
+#   ]
 
-  key_service_roles_for_autoscaling = [
-    # required for the ASG to manage encrypted volumes for nodes
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-    # required for the cluster / persistentvolume-controller to create encrypted PVCs
-    module.eks.cluster_iam_role_arn,
-  ]
+#   key_service_roles_for_autoscaling = [
+#     # required for the ASG to manage encrypted volumes for nodes
+#     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
+#     # required for the cluster / persistentvolume-controller to create encrypted PVCs
+#     module.eks.cluster_iam_role_arn,
+#   ]
 
-  # Aliases
-  aliases = ["eks/${local.name}/ebs"]
+#   # Aliases
+#   aliases = ["eks/${local.name}/ebs"]
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
