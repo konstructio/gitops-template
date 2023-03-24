edb9d391c78d7e1b555a7874e9f26818bafb3fb8
John Dietz <john@kubefirst.com>
2023-03-23 22:31:59 -0400

    remove

--- aws-gitlab/terraform/aws/eks/main.tf
+++ aws-gitlab/terraform/aws/eks/main.tf
@@ -540,27 +540,3 @@ resource "aws_iam_policy" "vault_server" {
 EOT
 }
 
-# module "ebs_kms_key" {
-#   source  = "terraform-aws-modules/kms/aws"
-#   version = "~> 1.5"
-
-#   description = "Customer managed key to encrypt EKS managed node group volumes"
-
-#   # Policy
-#   key_administrators = [
-#     data.aws_caller_identity.current.arn,
-#     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/atlantis-<CLUSTER_NAME>",
-#   ]
-
-#   key_service_roles_for_autoscaling = [
-#     # required for the ASG to manage encrypted volumes for nodes
-#     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-#     # required for the cluster / persistentvolume-controller to create encrypted PVCs
-#     module.eks.cluster_iam_role_arn,
-#   ]
-
-#   # Aliases
-#   aliases = ["eks/${local.name}/ebs"]
-
-#   tags = local.tags
-# }
