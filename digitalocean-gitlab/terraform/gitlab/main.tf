9c4961d63ea8bd259dbcd454eac5e7e917de09b7
Scott Hawkins <scott@echoboomer.net>
2023-04-25 09:16:56 -0400

    feat: add digitalocean gitlab

---
+++ digitalocean-gitlab/terraform/gitlab/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    endpoint = "<KUBEFIRST_STATE_STORE_BUCKET_HOSTNAME>"
+    key      = "terraform/gitlab/terraform.tfstate"
+    bucket   = "<KUBEFIRST_STATE_STORE_BUCKET>"
+    // Don't change this.
+    region = "us-east-1"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+  required_providers {
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "15.8.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
