889fae7359228b16d9a761137ef19d4177d7db49
Scott Hawkins <scott@echoboomer.net>
2023-05-30 23:07:06 -0400

    fix: pin to right version, remove incompatible field

--- aws-gitlab/terraform/aws/eks/main.tf
+++ aws-gitlab/terraform/aws/eks/main.tf
@@ -110,7 +110,7 @@ module "eks" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.1"
+  version = "4.0.2"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -120,9 +120,8 @@ module "vpc" {
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
   intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
-  enable_ipv6                     = true
-  assign_ipv6_address_on_creation = true
-  create_egress_only_igw          = true
+  enable_ipv6            = true
+  create_egress_only_igw = true
 
   public_subnet_ipv6_prefixes  = [0, 1, 2]
   private_subnet_ipv6_prefixes = [3, 4, 5]
