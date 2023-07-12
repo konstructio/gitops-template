2ecc4ce2df5dbf66f2b8e35d1ce4a9618a4f7272
Scott Hawkins <scott@echoboomer.net>
2023-07-12 19:36:40 -0400

    feat: gcp github

---
+++ gcp-github/terraform/gcp/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  cluster_name = "<CLUSTER_NAME>"
+}
+
+variable "gcp_region" {
+  description = "GCP Cloud Region"
+  type        = string
+
+  default = "<CLOUD_REGION>"
+}
+
+variable "network_name" {
+  description = "The name of the created network."
+  type        = string
+
+  default = "kubefirst"
+}
+
+variable "project" {
+  description = "GCP Project ID"
+  type        = string
+
+  default = "<GCP_PROJECT>"
+}
