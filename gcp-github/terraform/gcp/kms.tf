c89836e2599666f2bc8c1ad84f17fd08ff8eeaff
Clayton Dittman <Claywd@users.noreply.github.com>
2023-08-22 17:54:59 -0500

    fix: spelling

--- gcp-github/terraform/gcp/kms.tf
+++ gcp-github/terraform/gcp/kms.tf
@@ -3,7 +3,7 @@
 module "vault_keys" {
   source = "./modules/kms"
 
-  keyring  = "vault-${local.cluster_name}-${lower(var.uniquness)}"
+  keyring  = "vault-${local.cluster_name}-${lower(var.uniqueness)}"
   keys     = ["vault-unseal", "vault-encrypt"]
   location = "global"
   project  = var.project
