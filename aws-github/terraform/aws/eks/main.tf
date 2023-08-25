3e2e4659a25c7d7634772c0372a43e25653d1bfd
CristhianF7 <CristhianF7@gmail.com>
2023-08-25 09:15:18 -0500

    chore: rollback test

--- aws-github/terraform/aws/eks/main.tf
+++ aws-github/terraform/aws/eks/main.tf
@@ -120,7 +120,7 @@ module "vpc" {
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
   intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
-  enable_ipv6            = false
+  enable_ipv6            = true
   create_egress_only_igw = true
 
   public_subnet_ipv6_prefixes  = [0, 1, 2]
