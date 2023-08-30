3ef117b0c5523ef7cc93edaef0875996bf8ba665
Jared Edwards <jared@kubefirst.com>
2023-08-30 11:29:47 -0600

    fix atlantis path

---
+++ civo-github/terraform/civo/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  backend "s3" {
+    bucket   = "<KUBEFIRST_STATE_STORE_BUCKET>"
+    key      = "terraform/civo/terraform.tfstate"
+    endpoint = "https://objectstore.<CLOUD_REGION>.civo.com"
+
+    region = "<CLOUD_REGION>"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
+  }
+  required_providers {
+    civo = {
+      source = "civo/civo"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.23.0"
+    }
+    vault = {
+      source = "hashicorp/vault"
+      version = "3.19.0"
+    }
+  }
+}
+provider "civo" {
+  region = "<CLOUD_REGION>"
+}
+
+locals {
+  cluster_name = "<CLUSTER_NAME>"
+}
+
+resource "civo_network" "kubefirst" {
+  label = local.cluster_name
+}
+
+resource "civo_firewall" "kubefirst" {
+  name                 = local.cluster_name
+  network_id           = civo_network.kubefirst.id
+  create_default_rules = true
+}
+
+resource "civo_kubernetes_cluster" "kubefirst" {
+  name        = local.cluster_name
+  network_id  = civo_network.kubefirst.id
+  firewall_id = civo_firewall.kubefirst.id
+  pools {
+    label      = local.cluster_name
+    size       = "g4s.kube.medium"
+    node_count = 6
+  }
+}
