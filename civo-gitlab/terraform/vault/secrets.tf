29c2c6d9daf87ac8966b5a468729fbe1c57584b9
Jared Edwards <jared@kubefirst.com>
2023-09-14 16:35:48 -0600

    fix token ref

--- civo-gitlab/terraform/vault/secrets.tf
+++ civo-gitlab/terraform/vault/secrets.tf
@@ -67,7 +67,7 @@ resource "vault_generic_secret" "crossplane" {
       CIVO_TOKEN            = var.civo_token
       VAULT_ADDR            = "http://vault.vault.svc.cluster.local:8200"
       VAULT_TOKEN           = var.vault_token
-      password              = var.github_token
+      password              = var.gitlab_token
       username              = "kbot"
     }
   )
