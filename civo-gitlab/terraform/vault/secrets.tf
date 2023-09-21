9de61235cff5ff6e716ad0b59b3fc77a149b431a
John Dietz <john@kubefirst.com>
2023-09-20 23:54:36 -0400

    add crossplane secret to gitlab civo

--- civo-gitlab/terraform/vault/secrets.tf
+++ civo-gitlab/terraform/vault/secrets.tf
@@ -173,3 +173,21 @@ resource "vault_generic_secret" "atlantis_secrets" {
   )
   depends_on = [vault_mount.secret]
 }
+
+resource "vault_generic_secret" "crossplane" {
+  path = "secret/crossplane"
+
+  data_json = jsonencode(
+    {
+      AWS_ACCESS_KEY_ID     = var.aws_access_key_id,
+      AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key,
+      CIVO_TOKEN            = var.civo_token
+      VAULT_ADDR            = "http://vault.vault.svc.cluster.local:8200"
+      VAULT_TOKEN           = var.vault_token
+      password              = var.gitlab_token
+      username              = "kbot"
+    }
+  )
+
+  depends_on = [vault_mount.secret]
+}
