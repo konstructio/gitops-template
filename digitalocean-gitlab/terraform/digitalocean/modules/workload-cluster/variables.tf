8e58dd57442b794861124ca0dfaf1464d9482494
Jared Edwards <jared@kubefirst.com>
2023-11-30 09:42:31 -0700

    Digitalocean gitlab (#681)
    * fix digitalocean spaces endpoints and missed vultr token
    
    * cleanup folder, add module, fix gh atlantis path

---
+++ digitalocean-gitlab/terraform/digitalocean/modules/workload-cluster/variables.tf
@@ -0,0 +1,20 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+  default = ""
+}
+
+variable "node_type" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
