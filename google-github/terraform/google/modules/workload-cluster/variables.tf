3c64f8378c553a288facc6de47a701d991ca65b5
Jared Edwards <jared@kubefirst.com>
2023-12-09 16:24:44 -0700

    fix argocd ingress, remove harcode and adjust token

--- google-github/terraform/google/modules/workload-cluster/variables.tf
+++ google-github/terraform/google/modules/workload-cluster/variables.tf
@@ -1,43 +1,31 @@
 variable "cluster_name" {
   description = "cluster name"
   type        = string
-
-  default = "<CLUSTER_NAME>"
 }
 
 variable "environment" {
   description = "environment for the clusters"
   type        = string
-
-  default = "<CLUSTER_NAME>"
 }
 
 variable "google_region" {
   description = "Google Cloud Region"
   type        = string
-
-  default = "<CLOUD_REGION>"
 }
 
 variable "network_name" {
   description = "The name of the created network."
   type        = string
-
-  default = "<CLUSTER_NAME>"
 }
 
 variable "node_count" {
   description = "The node count per zone for the cluster."
   type        = string
-
-  default = "<NODE_COUNT>"
 }
 
 variable "project" {
   description = "Google Project ID"
   type        = string
-
-  default = "<GOOGLE_PROJECT>"
 }
 
 variable "force_destroy" {
