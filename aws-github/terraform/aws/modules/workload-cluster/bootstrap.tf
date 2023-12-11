975ab46cb7b5f9299d5bcef26630c0dbc52a547a
Jared Edwards <jared@kubefirst.com>
2023-12-10 21:14:42 -0700

    tokens for aws account and workload cluster

--- aws-github/terraform/aws/modules/workload-cluster/bootstrap.tf
+++ aws-github/terraform/aws/modules/workload-cluster/bootstrap.tf
@@ -21,7 +21,7 @@ resource "vault_generic_secret" "clusters" {
       host                    = module.eks.cluster_endpoint
       cluster_name            = var.cluster_name
       environment             = var.cluster_name
-      argocd_role_arn         = "arn:aws:iam::126827061464:role/argocd-kray-one"
+      argocd_role_arn         = "arn:aws:iam::<AWS_ACCOUNT_ID>:role/argocd-<WORKLOAD_CLUSTER_NAME>"
     }
   )
   depends_on = [ module.eks ]
