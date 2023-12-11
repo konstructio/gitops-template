e18cf1d04eba932672706b0f9ba1494411cf2721
Jared Edwards <jared@kubefirst.com>
2023-12-10 21:28:53 -0700

    adjsut token and node count

--- aws-github/terraform/aws/modules/workload-cluster/main.tf
+++ aws-github/terraform/aws/modules/workload-cluster/main.tf
@@ -1,16 +1,3 @@
-
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data != null ? module.eks.cluster_certificate_authority_data : "")
-
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     # This requires the awscli to be installed locally where Terraform is executed
-#     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#   }
-# }
-
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
