ba28e7f956e32233b0ef1973dce0d7ca146eb3c0
Jared Edwards <jared@kubefirst.com>
2023-12-10 18:03:19 -0700

    initial workload cluster module and physical template

---
+++ aws-github/terraform/aws/modules/workload-cluster/bootstrap.tf
@@ -0,0 +1,137 @@
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data != null ? module.eks.cluster_certificate_authority_data : "")
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+resource "vault_generic_secret" "clusters" {
+  path = "secret/clusters/${var.cluster_name}"
+
+  data_json = jsonencode(
+    {
+      cluster_ca_certificate  = base64decode(module.eks.cluster_certificate_authority_data)
+      host                    = module.eks.cluster_endpoint
+      cluster_name            = var.cluster_name
+      environment             = var.cluster_name
+      argocd_role_arn         = "arn:aws:iam::126827061464:role/argocd-kray-one"
+    }
+  )
+  depends_on = [ module.eks ]
+}
+
+resource "kubernetes_namespace_v1" "external_dns" {
+  metadata {
+    name = "external-dns"
+  }
+}
+
+data "vault_generic_secret" "external_dns" {
+  path = "secret/external-dns"
+}
+
+resource "kubernetes_secret_v1" "external_dns" {
+  metadata {
+    name = "external-dns-secrets"
+    namespace = kubernetes_namespace_v1.external_dns.metadata.0.name
+  }
+  data = {
+    token = data.vault_generic_secret.external_dns.data["token"]
+  }
+  type = "Opaque"
+}
+
+resource "kubernetes_namespace_v1" "external_secrets_operator" {
+  metadata {
+    name = "external-secrets-operator"
+  }
+}
+
+resource "kubernetes_namespace_v1" "environment" {
+  metadata {
+    name = var.cluster_name
+  }
+}
+
+data "vault_generic_secret" "docker_config" {
+  path = "secret/dockerconfigjson"
+}
+
+resource "kubernetes_secret_v1" "image_pull" {
+  metadata {
+    name      = "docker-config"
+    namespace = kubernetes_namespace_v1.environment.metadata.0.name
+  }
+
+  data = {
+    ".dockerconfigjson" = data.vault_generic_secret.docker_config.data["dockerconfig"]
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
+
+data "vault_generic_secret" "external_secrets_operator" {
+  path = "secret/atlantis"
+}
+
+resource "kubernetes_secret_v1" "external_secrets_operator_environment" {
+  metadata {
+    name      = "${var.cluster_name}-cluster-vault-bootstrap"
+    namespace = kubernetes_namespace_v1.environment.metadata.0.name
+  }
+  data = {
+    vault-token = data.vault_generic_secret.external_secrets_operator.data["VAULT_TOKEN"]
+  }
+  type = "Opaque"
+}
+
+resource "kubernetes_secret_v1" "external_secrets_operator" {
+  metadata {
+    name      = "${var.cluster_name}-cluster-vault-bootstrap"
+    namespace = kubernetes_namespace_v1.external_secrets_operator.metadata.0.name
+  }
+  data = {
+    vault-token = data.vault_generic_secret.external_secrets_operator.data["VAULT_TOKEN"]
+  }
+  type = "Opaque"
+}
+
+resource "kubernetes_service_account_v1" "external_secrets" {
+  metadata {
+    name      = "external-secrets"
+    namespace = kubernetes_namespace_v1.external_secrets_operator.metadata.0.name
+  }
+  secret {
+    name = "external-secrets-token"
+  }
+}
+
+resource "kubernetes_secret_v1" "external_secrets" {
+  metadata {
+    name      = "external-secrets-token"
+    namespace = kubernetes_namespace_v1.external_secrets_operator.metadata.0.name
+    annotations = {
+      "kubernetes.io/service-account.name" = "external-secrets"
+    }
+  }
+  type       = "kubernetes.io/service-account-token"
+  depends_on = [kubernetes_service_account_v1.external_secrets]
+}
+
+resource "kubernetes_config_map" "kubefirst_cm" {
+  metadata {
+    name      = "kubefirst-cm"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mgmt_cluster_id = "x8x68z"
+  }
+}
