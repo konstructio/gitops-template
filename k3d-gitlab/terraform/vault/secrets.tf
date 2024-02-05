af8f1339c0b7623ea6a9d1bb34d8bedd57419270
John Dietz <john@kubefirst.com>
2024-02-05 18:16:39 -0500

    adding crossplane secret to k3d stacks (#702)

--- k3d-gitlab/terraform/vault/secrets.tf
+++ k3d-gitlab/terraform/vault/secrets.tf
@@ -193,3 +193,20 @@ resource "vault_generic_secret" "atlantis_secrets" {
 
   depends_on = [vault_mount.secret]
 }
+
+resource "vault_generic_secret" "crossplane" {
+  path = "secret/crossplane"
+
+  data_json = jsonencode(
+    {
+      AWS_ACCESS_KEY_ID     = var.aws_access_key_id,
+      AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key,
+      VAULT_ADDR            = "http://vault.vault.svc.cluster.local:8200"
+      VAULT_TOKEN           = var.vault_token
+      password              = var.gitlab_token
+      username              = "<GITLAB_USER>"
+    }
+  )
+
+  depends_on = [vault_mount.secret]
+}
