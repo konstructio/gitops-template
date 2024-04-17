6727b2e8f51273b27b0efe1c987acf28a36a4a18
FalcoSuessgott <tommorelly@gmail.com>
2024-04-18 00:21:07 +0700

    fix(tffmt): fix CI and run terraform fmt (#744)

--- akamai-github/terraform/akamai/main.tf
+++ akamai-github/terraform/akamai/main.tf
@@ -13,15 +13,15 @@ terraform {
   }
   required_providers {
     linode = {
-      source = "linode/linode"
+      source  = "linode/linode"
       version = "2.16.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.23.0"
     }
     vault = {
-      source = "hashicorp/vault"
+      source  = "hashicorp/vault"
       version = "3.19.0"
     }
   }
@@ -29,26 +29,26 @@ terraform {
 provider "linode" {}
 
 locals {
-  cluster_name = "<CLUSTER_NAME>"
+  cluster_name         = "<CLUSTER_NAME>"
   kube_config_filename = "../../../kubeconfig"
 }
 
 resource "linode_lke_cluster" "kubefirst" {
-    label       = local.cluster_name
-    k8s_version = "1.28"
-    region      = "us-central"
-    tags        = ["<CLUSTER_NAME>"]
+  label       = local.cluster_name
+  k8s_version = "1.28"
+  region      = "us-central"
+  tags        = ["<CLUSTER_NAME>"]
 
-    pool {
-        # NOTE: If count is undefined, the initial node count will
-        # equal the minimum autoscaler node count.
-        type  = "<NODE_TYPE>" # "g6-standard-4" 4
+  pool {
+    # NOTE: If count is undefined, the initial node count will
+    # equal the minimum autoscaler node count.
+    type = "<NODE_TYPE>" # "g6-standard-4" 4
 
-        autoscaler {
-          min = tonumber("<NODE_COUNT>") # tonumber() is used for a string token value
-          max = tonumber("<NODE_COUNT>") # tonumber() is used for a string token value
-        }
+    autoscaler {
+      min = tonumber("<NODE_COUNT>") # tonumber() is used for a string token value
+      max = tonumber("<NODE_COUNT>") # tonumber() is used for a string token value
     }
+  }
 }
 
 resource "local_file" "kubeconfig" {
