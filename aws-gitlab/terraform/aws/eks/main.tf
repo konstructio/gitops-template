a21b44857687dde8905c539ea462e74a573f5294
jokestax <mrrishi373@gmail.com>
2024-07-25 06:22:16 +0000

    version change

--- aws-gitlab/terraform/aws/eks/main.tf
+++ aws-gitlab/terraform/aws/eks/main.tf
@@ -74,6 +74,7 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
+
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["<NODE_TYPE>"]
