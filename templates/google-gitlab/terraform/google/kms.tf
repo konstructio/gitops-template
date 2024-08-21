eadaad93714a7a4f1705d251833f6392834f989d
Simon Emms <simon@simonemms.com>
2024-08-21 12:27:53 +0000

    refactor: move google terraform config

--- templates/google-gitlab/terraform/google/kms.tf
+++
@@ -1,8 +0,0 @@
-module "vault_keys" {
-  source = "./modules/kms"
-
-  keyring  = "vault-${local.cluster_name}-${lower(var.uniqueness)}"
-  keys     = ["vault-unseal", "vault-encrypt"]
-  location = "global"
-  project  = var.project
-}
