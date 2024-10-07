cc7e1ad95f6c3939190c653c910945bc1512550d
mrrishi <mrrishi373@gmail.com>
2024-10-08 02:48:34 +0530

    add eks cluster autoscaler

--- aws-github/terraform/aws/eks/main.tf
+++ aws-github/terraform/aws/eks/main.tf
@@ -688,3 +688,37 @@ resource "aws_iam_policy" "vault_server" {
 }
 EOT
 }
+
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name = "cluster-autoscaler"
+  path = "/"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeScalingActivities",
+          "ec2:DescribeImages",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:GetInstanceTypesFromInstanceRequirements",
+          "eks:DescribeNodegroup"
+        ],
+        "Resource": ["*"]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup"
+        ],
+        "Resource": ["*"]
+      }
+    ]
+  })
+}
--- aws-github/terraform/aws/eks/outputs.tf
+++ aws-github/terraform/aws/eks/outputs.tf
@@ -5,3 +5,7 @@ output "node_iam_role_name" {
 output "external_dns_policy_arn" {
   value = aws_iam_policy.external_dns.arn
 }
+
+output "cluster_autoscaler_policy_arn" {
+  value = aws_iam_policy.cluster_autoscaler.arn
+}
