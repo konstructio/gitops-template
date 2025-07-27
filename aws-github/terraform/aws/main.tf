4dbb55d53873cbf217fa685e7b530e45416965d1
John Dietz <john@konstruct.io>
2025-07-27 19:55:26 -0400

    aws provider versions sorted

--- aws-github/terraform/aws/main.tf
+++ aws-github/terraform/aws/main.tf
@@ -8,6 +8,12 @@ terraform {
   }
 }
 
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = ">= 5.61.0"  # This allows v6.x
+  }
+}
 
 provider "aws" {
   region = var.aws_region
