c077650b2928a483480cd95a5b1d051759c3016e
John Dietz <john@konstruct.io>
2025-07-28 15:00:46 -0400

    patch bugfix in required providers

--- aws-gitlab/terraform/aws/main.tf
+++ aws-gitlab/terraform/aws/main.tf
@@ -6,12 +6,12 @@ terraform {
     region  = "<CLOUD_REGION>"
     encrypt = true
   }
-}
 
-required_providers {
-  aws = {
-    source  = "hashicorp/aws"
-    version = ">= 5.61.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.61.0"
+    }
   }
 }
 
