52abdc542b1d2937ffde8e64fcdd7c3026b707a0
John Dietz <john@konstruct.io>
2025-07-28 14:41:17 -0400

    adjust gitlab aws to match github stack changeset

--- aws-github/terraform/aws/main.tf
+++ aws-github/terraform/aws/main.tf
@@ -11,7 +11,7 @@ terraform {
 required_providers {
   aws = {
     source  = "hashicorp/aws"
-    version = ">= 5.61.0"  # This allows v6.x
+    version = ">= 5.61.0"
   }
 }
 
--- aws-gitlab/terraform/aws/main.tf
+++ aws-gitlab/terraform/aws/main.tf
@@ -8,6 +8,13 @@ terraform {
   }
 }
 
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = ">= 5.61.0"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
   default_tags {
