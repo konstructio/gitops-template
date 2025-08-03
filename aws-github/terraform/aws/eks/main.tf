597a9a414e4dd5a1e7b1f9d880f7019690b5fb4b
mrrishi <mrrishi373@gmail.com>
2025-08-03 10:04:39 +0530

    feat: add eni config

--- aws-github/terraform/aws/eks/main.tf
+++ aws-github/terraform/aws/eks/main.tf
@@ -23,6 +23,7 @@ locals {
   region          = "<CLOUD_REGION>"
 
   vpc_cidr = "10.0.0.0/16"
+  secondary_vpc_cidr =  ""
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
@@ -70,6 +71,7 @@ module "eks" {
           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
           ENABLE_PREFIX_DELEGATION = "true"
           WARM_PREFIX_TARGET       = "1"
+          AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG = "true"
         }
       })
     }
@@ -154,9 +156,13 @@ module "vpc" {
 
   name = local.name
   cidr = local.vpc_cidr
-
+  secondary_cidr_blocks = local.secondary_vpc_cidr
   azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  # private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  private_subnets = concat(
+    [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)],
+    [for k, v in local.azs : cidrsubnet(local.secondary_vpc_cidr, 2, k)]
+  )
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
   intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
@@ -180,6 +186,24 @@ module "vpc" {
   }
 
   tags = local.tags
+}
+
+resource "kubectl_manifest" "eni_config" {
+for_each = zipmap(local.azs, slice(module.vpc.private_subnets, 3, 6))
+
+yaml_body = yamlencode({
+   apiVersion = "crd.k8s.amazonaws.com/v1alpha1"
+   kind       = "ENIConfig"
+   metadata = {
+      name = each.key
+   }
+   spec = {
+      securityGroups = [
+      module.eks.node_security_group_id,
+      ]
+      subnet = each.value
+   }
+})
 }
 
 module "vpc_cni_irsa" {
